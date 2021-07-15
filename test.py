5500dcc41a553fe5563dbc2210deba535e77e31e
Felipe Sens Bonetto <felipesensbonetto@gmail.com>
2021-07-15 17:18:18 -0300

    print requests

--- test.py
+++ test.py
@@ -12,14 +12,14 @@ def create(arg):
   data = {arg[0]: arg[1]}
   headers = {"Content-Type": "application/json"}
   request = requests.post(url, data=json.dumps(data), headers=headers)
-  print(request.json())
+  print(f"{request.json()}, {arg[0]}")
 
 def read(arg):
   headers = {"Content-Type": "application/json"}
   request = requests.get(f"{url}/{arg[0]}", headers=headers)
   print(request.json())
 
-to_be_created = [(uuid.uuid4().hex, uuid.uuid4().hex) for i in range(100)]
+to_be_created = [(uuid.uuid4().hex, uuid.uuid4().hex) for i in range(1000)]
 
 pool.map(create, to_be_created)
 pool.map(read, to_be_created)
